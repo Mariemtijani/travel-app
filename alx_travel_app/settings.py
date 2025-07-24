3d6d9c123b90e6e9047dd50aeba512f09273f0e3
Mariem Tijani <mariemtijani60@gmail.com>
2025-07-24 23:18:28 +0100

    commit

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mariem1998.pythonanywhere.com']
 
 
 # Application definition
