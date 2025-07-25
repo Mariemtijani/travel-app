87a5738258a2c7390971800a3fa150f83ad3d66e
Mariem Tijani <mariemtijani60@gmail.com>
2025-07-25 09:01:30 +0100

    commit

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     path('', include('listings.urls')),
 
         # Swagger UI:
-    # re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
     # Redoc UI (optional):
